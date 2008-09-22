1a00f4bb2b5908f4bcd09094c44d31193adc68ca
fjgomez <fjgomez@d64dce68-0152-0410-9b99-1d260748578e>
2008-09-22 12:54:34 +0000

    Poniendo a punto install.py

--- trunk/install.py
+++ trunk/install.py
@@ -6,77 +6,131 @@
 # TO-DO: Considerar también la situación en que ya existe una instalación
 # y se desea preservar los datos locales.
 
+def run(command, msg = 'Error'):
+    # FIXME! (see update-opac.py)
+    ENV = {'PATH': os.getenv('PATH') + os.pathsep + 'G:\\programas\\cisis\\5.2\\1030'}  # CONFIG.get('Global', 'PATH_CISIS')
+    return run_command(command, msg = msg, env = ENV)
 
 def set_version():
-    # Genera un identificador de la versión y lo inserta en opac-footer.htm
+    # Genera un identificador de la versión y lo inserta en el footer.
+    # svnversion produce identificadores de la forma '322' o '322M'.
+    # En el 2do caso, significa que se trata de la revisión 322 con
+    # modificaciones locales.
     version = os.popen('svnversion').read()
-    footer_file = file('cgi-bin/opac/html/opac-footer.htm')
-    footer_file.write(
-        footer_file.read().replace('__VERSION__', 'rev. %s' % version)
+    footer_file = file(FILES['footer'])
+    aux_file = file('footer.tmp', 'w')
+    aux_file.write(
+        footer_file.read().replace('__VERSION__', '(rev. %s)' % version)
     )
+    aux_file.close()
+    shutil.move('footer.tmp', FILES['footer'])
+    
+    print "Identificador de version generado."
 
-def create_db():
-    # Crea las bases isis auxiliares a partir de archivos de texto (.id)
-    # TO-DO: ajustar saltos de línea (usar os.linesep?)?
-    run_command('id2i bases/id/country.id create=bases/common/country')
-    run_command('id2i bases/id/lang.id create=bases/common/lang')
-    run_command('id2i bases/id/dictgiz.id create=bases/common/dictgiz')
-    run_command('id2i bases/id/biblio.id create=admin/work/demo/original/biblio')
-
-    # Genera los invertidos correspondientes
-    run_command('mx bases/common/country "fst=1 0 v1" fullinv=bases/common/country')
-    run_command('mx bases/common/lang "fst=1 0 v1" fullinv=bases/common/lang')
-
-# Cambiar saltos de línea en archivos .tab (usar os.linesep?)
+def replace_config_path(config_file):
+    # Crea un archivo de configuración a partir de una plantilla y del valor
+    # actual de OPACMARC_DIR.
+    if os.path.isfile(config_file):
+        print
+        print "ATENCION: ya existe el archivo de configuracion %s." % os.path.abspath(config_file)
+    else:
+        try:
+            f1 = open(config_file + '.dist', 'r')
+            f2 = open(config_file, 'w')
+            #for line in f1: f2.write(line.replace('__OPACMARC_DIR__', OPACMARC_DIR))
+            f2.write(
+                f1.read().replace('__OPACMARC_DIR__', OPACMARC_DIR)
+            )
+            f1.close()
+            f2.close()
+            print
+            print 'Generado el archivo %s.' % os.path.abspath(config_file)
+        except:
+            print
+            print "ERROR: No se pudo generar el archivo %s." % os.path.abspath(config_file)
 
 def set_config():
-    # Ajusta los paths en los archivos de configuración a partir de plantillas.
+    # Crea archivos de configuración.
+    replace_config_path(FILES['cipar'])
+    replace_config_path(FILES['httpd'])
+    replace_config_path(FILES['local'])
 
-    # Tomamos como base un cipar incluido en la distribución y lo adecuamos a nuestro OPACMARC_DIR.
-    # Hay que usar el path *absoluto* para el cipar
-    CIPAR = os.path.join(OPACMARC_DIR, 'opac', 'opac.cip')
-    try:
-        f1 = open(CIPAR + '.dist', 'r')  # archivo CIPAR de la distribución
-        f2 = open(CIPAR, 'w')
-        #for line in f1: f2.write(line.replace('__OPACMARC_DIR__', OPACMARC_DIR))
-        f2.write(
-            f1.read().replace('__OPACMARC_DIR__', OPACMARC_DIR)
-        )
-        f1.close()
-        f2.close()
-    except:
-        error("No se pudo generar el archivo cipar.")
-
-    # TO-DO: lo mismo con local.conf, httpd-opacmarc.conf
-
-def create_dirs():    
+def create_dirs():
     # En Windows crear directorio temp para búsquedas de wxis (también en Linux para cache?), y ajustar config.
     # No necesitamos tener ese dir en el repositorio; svn:ignore temp
-    os.mkdir('temp')
+    try:
+        os.mkdir('temp')
+        print "Directorio temp creado."
+    except:
+        print
+        print "ATENCION: No se pudo crear la carpeta temp."
       
     # Crear directorio logs, e incluir dentro de él un README?
     # No necesitamos tener ese dir en el repositorio; svn:ignore logs
-    os.mkdir('logs')
+    #try:
+    #   os.mkdir('logs')
+    #   print "Directorio logs creado."
+    #except:
+    #   print
+    #   print "ATENCION: No se pudo crear la carpeta logs."
+
+def create_db():
+
+    # Crea las bases isis auxiliares a partir de archivos de texto (.id)
+    # FIXME: ajustar saltos de línea de los .id (usar os.linesep?)
+    run('id2i bases/id/country.id create=bases/common/country')
+    run('id2i bases/id/lang.id create=bases/common/lang')
+    run('id2i bases/id/dictgiz.id create=bases/common/dictgiz')
+    run('id2i bases/id/demo.id create=admin/work/demo/original/biblio')
+
+    # Genera los invertidos correspondientes
+    run('mx bases/common/country "fst=1 0 v1" fullinv=bases/common/country')
+    run('mx bases/common/lang "fst=1 0 v1" fullinv=bases/common/lang')
+    
+    print
+    print "Bases auxiliares creadas."
+
+# FIXME - Cambiar saltos de línea en archivos .tab (usar os.linesep?)
 
 def show_msg():    
     # Mostrar mensajes útiles para el usuario (tips, tareas que debe realizar luego de instalar)
-    print "Mensajes"
+    print
+    print "Algunos mensajes para el admin."
 
 # Ejecutar newdb.py con la base demo?
 # Ejecutar update-opac.py para la base demo?
   
 # Realizar tests? E.g. búsquedas con acentos y agrep.
 
+# ---------------------
+# MAIN
+# ---------------------
 
 import os
 import sys
+import shutil
 
 OPACMARC_DIR = os.path.abspath(os.path.dirname(sys.argv[0]))
 sys.path.insert(0, os.path.join(OPACMARC_DIR, 'util'))
 from util import run_command, error
 
+FILES = {
+    'footer' : os.path.join(OPACMARC_DIR, 'cgi-bin', 'opac', 'html', 'opac-footer.htm'),
+    'cipar'  : os.path.join(OPACMARC_DIR, 'admin', 'opac', 'opac.cip'),
+    'httpd'  : os.path.join(OPACMARC_DIR, 'httpd-opacmarc.conf'),
+    'local'  : os.path.join(OPACMARC_DIR, 'config', 'local.conf'),
+}
+
+# FIXME - add cisis path to env
+
+print '''
+-----------------------------------------------------
+  install.py - SCRIPT DE INSTALACION DE OPACMARC
+-----------------------------------------------------
+'''
+
 set_version()
-create_db()
 set_config()
 create_dirs()
+create_db()
 show_msg()
