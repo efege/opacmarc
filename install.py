8f303f8a55d2ab6dec2d31eb1231e6b763c46bc3
fjgomez <fjgomez@d64dce68-0152-0410-9b99-1d260748578e>
2008-09-22 19:17:21 +0000

    Mejoras en install.py

--- install.py
+++ install.py
@@ -1,9 +1,9 @@
 # coding=windows-1252
 
-# Script de instalaciÃ³n para OpacMarc
+# Script de instalación para OpacMarc
 # Issue 10: http://code.google.com/p/opacmarc/issues/detail?id=10
 
-# TO-DO: Considerar tambiÃ©n la situaciÃ³n en que ya existe una instalaciÃ³n
+# TO-DO: Considerar también la situación en que ya existe una instalación
 # y se desea preservar los datos locales.
 
 def run(command, msg = 'Error'):
@@ -12,11 +12,11 @@ def run(command, msg = 'Error'):
     return run_command(command, msg = msg, env = ENV)
 
 def set_version():
-    # Genera un identificador de la versiÃ³n y lo inserta en el footer.
+    # Genera un identificador de la versión y lo inserta en el footer.
     # svnversion produce identificadores de la forma '322' o '322M'.
-    # En el 2do caso, significa que se trata de la revisiÃ³n 322 con
+    # En el 2do caso, significa que se trata de la revisión 322 con
     # modificaciones locales.
-    version = os.popen('svnversion').read()
+    version = os.popen('svnversion').read().replace(os.linesep, '')
     footer_file = file(FILES['footer'])
     aux_file = file('footer.tmp', 'w')
     aux_file.write(
@@ -28,7 +28,7 @@ def set_version():
     print "Identificador de version generado."
 
 def replace_config_path(config_file):
-    # Crea un archivo de configuraciÃ³n a partir de una plantilla y del valor
+    # Crea un archivo de configuración a partir de una plantilla y del valor
     # actual de OPACMARC_DIR.
     if os.path.isfile(config_file):
         print
@@ -37,37 +37,38 @@ def replace_config_path(config_file):
         try:
             f1 = open(config_file + '.dist', 'r')
             f2 = open(config_file, 'w')
-            #for line in f1: f2.write(line.replace('__OPACMARC_DIR__', OPACMARC_DIR))
             f2.write(
                 f1.read().replace('__OPACMARC_DIR__', OPACMARC_DIR)
             )
             f1.close()
             f2.close()
             print
-            print 'Generado el archivo %s.' % os.path.abspath(config_file)
+            print 'Generado el archivo %s.' % config_file
         except:
             print
-            print "ERROR: No se pudo generar el archivo %s." % os.path.abspath(config_file)
+            print "ERROR: No se pudo generar el archivo %s." % config_file
 
 def set_config():
-    # Crea archivos de configuraciÃ³n.
-    replace_config_path(FILES['cipar'])
-    replace_config_path(FILES['httpd'])
-    replace_config_path(FILES['local'])
-    replace_config_path(FILES['update'])
+    # Crea archivos de configuración.
+    replace_config_path(FILES['httpd'])   # modelo de config. para Apache
+    replace_config_path(FILES['local'])   # config. local (para opac.xis) 
+    replace_config_path(FILES['update'])  # para update-opac.py
+    replace_config_path(FILES['cipar'])   # para las llamadas a mx desde update-opac.py
 
 def create_dirs():
-    # En Windows crear directorio temp para bÃºsquedas de wxis (tambiÃ©n en Linux para cache?), y ajustar config.
+    # En Windows crear directorio temp para búsquedas de wxis (también en Linux para cache?), y ajustar config.
     # No necesitamos tener ese dir en el repositorio; svn:ignore temp
     try:
         os.mkdir('temp')
         print
         print "Directorio temp creado."
     except:
-        print
-        print "ATENCION: No se pudo crear la carpeta temp."
+        pass
+        # posiblemente ya existe el dir. temp
+        #print
+        #print "ATENCION: No se pudo crear la carpeta temp."
       
-    # Crear directorio logs, e incluir dentro de Ã©l un README?
+    # Crear directorio logs, e incluir dentro de él un README?
     # No necesitamos tener ese dir en el repositorio; svn:ignore logs
     #try:
     #   os.mkdir('logs')
@@ -77,9 +78,9 @@ def create_dirs():
     #   print "ATENCION: No se pudo crear la carpeta logs."
 
 def create_db():
-
     # Crea las bases isis auxiliares a partir de archivos de texto (.id)
-    # FIXME: ajustar saltos de lÃ­nea de los .id (usar os.linesep?)
+    # FIXME: ajustar saltos de línea de los .id (usar os.linesep?)
+    # En Linux hay problemas si usan '\r\n', pero en Windows pueden usar '\n'
     run('id2i bases/id/country.id create=bases/common/country')
     run('id2i bases/id/lang.id create=bases/common/lang')
     run('id2i bases/id/dictgiz.id create=bases/common/dictgiz')
@@ -97,23 +98,28 @@ def create_table(table_type):
     f = open(FILES[table_type], 'w')
     values = list(getattr(tablas, table_type))
     while values:
-        for val in values[:32]:
-            f.write(val + ' ')
-        f.write(os.linesep)
+        f.write(' '.join(values[:32]) + os.linesep)
         values = values[32:]
     f.close()
     print
     print "Tabla %s creada." % table_type
 
 def show_msg():    
-    # Mostrar mensajes Ãºtiles para el usuario (tips, tareas que debe realizar luego de instalar)
+    # Mostrar mensajes útiles para el usuario (tips, tareas que debe realizar luego de instalar)
     print
-    print "Algunos mensajes para el admin."
+    print '''
+-----------------------------------------------------
+  INSTALACION FINALIZADA
+-----------------------------------------------------
+'''
+    print '''Algunos mensajes para el admin:
+        - configurar permiso de escritura en temp y logs
+        - ejecutar newdb.py con la base demo?
+        - ejecutar update-opac.py demo
+        - wxis, agrep?
+        - Realizar tests? E.g. búsquedas con acentos y agrep.
+    '''
 
-# Ejecutar newdb.py con la base demo?
-# Ejecutar update-opac.py para la base demo?
-  
-# Realizar tests? E.g. bÃºsquedas con acentos y agrep.
 
 
 # ---------------------
@@ -129,8 +135,8 @@ sys.path.insert(0, os.path.join(OPACMARC_DIR, 'util'))
 from util import run_command, error
 import tablas
 
-# Archivos que crea o modifica el script de instalaciÃ³n.
-# TO-DO: agregar aquÃ­ los de create_db()
+# Archivos que crea o modifica el script de instalación.
+# TO-DO: agregar aquí los que usa create_db()
 FILES = {
     'footer' : os.path.join(OPACMARC_DIR, 'cgi-bin', 'opac', 'html', 'opac-footer.htm'),
     'cipar'  : os.path.join(OPACMARC_DIR, 'config', 'opac.cip'),
